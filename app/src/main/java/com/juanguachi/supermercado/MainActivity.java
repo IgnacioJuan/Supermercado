ca8994aa523df8f2a959ca9fe27e78fe90a15b05
IgnacioJuan <juan.guachichullca.est@tecazuay.edu.ec>
2022-10-04 22:43:43 -0500

    Seleccion de imagen

--- app/src/main/java/com/juanguachi/supermercado/MainActivity.java
+++ app/src/main/java/com/juanguachi/supermercado/MainActivity.java
@@ -1,13 +1,18 @@
 package com.juanguachi.supermercado;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
@@ -17,9 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
     //
     EditText txtruc, txtnombre, txtrepresentante, txtdireccion, txttelefono, txtproductos, txtcredito;
-    Button btnagregar, btnactualizar, btneliminar;
+    Button btnagregar, btnactualizar, btneliminar, btnSeleccionar;
 
-    //ImageView imagen;
+    ImageView imagen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,17 +47,41 @@ public class MainActivity extends AppCompatActivity {
         btnagregar = (Button) findViewById(R.id.btn_agregar);
         btnactualizar = (Button) findViewById(R.id.btn_editar);
         btneliminar = (Button) findViewById(R.id.btn_eliminar);
+        btnSeleccionar= (Button) findViewById(R.id.btn_SeleccionarImagen);
         //Imagen
-
+        imagen = (ImageView) findViewById(R.id.iv_imagen);
         //eventos
         btnagregar.setOnClickListener(guardarListener);
         btnactualizar.setOnClickListener(actualizarListener);
         btneliminar.setOnClickListener(eliminarListener);
-
+        btnSeleccionar.setOnClickListener(Seleccionarimagen);
     }
 
 
     //Listeners
+
+    View.OnClickListener Seleccionarimagen= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            cargarImagen();
+        }
+    };
+
+    private void cargarImagen() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"Seleccione la Aplicación"),10);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+            Uri path=data.getData();
+            imagen.setImageURI(path);
+        }
+    }
+
     View.OnClickListener guardarListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
